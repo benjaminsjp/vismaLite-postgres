b8ce44e2af1cd63e75759e284003f24a061c264a
benjaminsjp <114567951+benjaminsjp@users.noreply.github.com>
2024-01-26 14:57:34 +0100

    Update page.tsx

--- app/student/page.tsx
+++ app/student/page.tsx
@@ -1,20 +1,5 @@
 import { sql } from "@vercel/postgres";
 
-export default async function Cart({
-  params,
-}: {
-  params: { FNavn: string };
-}): Promise<JSX.Element> {
-  const { rows } =
-    await sql`SELECT * from public.elev where eId=${params.FNavn}`;
-
-  return (
-    <div>
-      {rows.map((row) => (
-        <div key={row.id}>
-          {row.id} - {row.quantity}
-        </div>
-      ))}
-    </div>
-  );
+export default async function student() {
+  return <h1>hello world</h1>;
 }
